b08df1215b591ffb38579481ce7ac3dfb3f00cfc
Jill Heske <jill.heske@udacity.com>
2019-01-09 13:21:08 -0500

    starter code

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModelFactory.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModelFactory.kt
@@ -1,5 +1,3 @@
-package com.example.android.trackmysleepquality.sleeptracker
-
 /*
  * Copyright 2018, The Android Open Source Project
  *
@@ -7,7 +5,7 @@ package com.example.android.trackmysleepquality.sleeptracker
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +13,8 @@ package com.example.android.trackmysleepquality.sleeptracker
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.ViewModel
@@ -37,3 +37,4 @@ class SleepTrackerViewModelFactory(
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
+
