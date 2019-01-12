ca65b93ca1e1adf47ed49ca76702ccf61d2482bd
Jill Heske <jill.heske@udacity.com>
2019-01-12 17:19:19 -0500

    Step.02-Exercise-Create-SleepDatabaseDao

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,20 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+// TODO (01) Create annotated interface SleepDatabaseDao.
+interface SleepDatabaseDao {
+
+// TODO (02) Add annotated insert() method for inserting a single SleepNight.
+
+// TODO (03) Add annotated update() method for updating a SleepNight.
+
+// TODO (04) Add annotated get() method that gets the SleepNight by key.
+
+// TODO (05) Add annotated clear() method and query.
+
+// TODO (06) Add annotated getAllNights() method and query.
+
+// TODO (07) Add annotated getTonight() method and query.
+
+}
+
