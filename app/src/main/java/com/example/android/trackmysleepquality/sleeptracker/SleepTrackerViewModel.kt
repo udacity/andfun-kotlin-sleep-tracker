6868204c7e7b70f6b89963715fb42edbcdd4af80
Doilio Matsinhe <doiliomatsinhe@gmail.com>
2020-06-15 22:07:39 +0200

    Update SleepTrackerViewModel.kt

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -135,6 +135,8 @@ class SleepTrackerViewModel(
             oldNight.endTimeMilli = System.currentTimeMillis()
 
             update(oldNight)
+            // Clear this value to avoid updating the latest night
+            tonight.value = null
         }
     }
 
