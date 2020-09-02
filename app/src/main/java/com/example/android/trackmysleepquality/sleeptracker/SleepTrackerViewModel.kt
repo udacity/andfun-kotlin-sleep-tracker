9b22adedc24736098976fa81d9e259ac47155ee6
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-09-02 22:36:23 +0530

    Revert "SleepTrackerViewModel.kt - Use ViewmodelScope instead of creating coroutine scope"
    This reverts commit 02149830a1b373508d8615ddf5ce5d4f0bcf2b27.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -34,12 +34,21 @@ class SleepTrackerViewModel(
         application: Application) : AndroidViewModel(application) {
 
     /**
+     * viewModelJob allows us to cancel all coroutines started by this ViewModel.
      */
+    private var viewModelJob = Job()
 
     /**
+     * A [CoroutineScope] keeps track of all coroutines started by this ViewModel.
      *
+     * Because we pass it [viewModelJob], any coroutine started in this uiScope can be cancelled
+     * by calling `viewModelJob.cancel()`
      *
+     * By default, all coroutines started in uiScope will launch in [Dispatchers.Main] which is
+     * the main thread on Android. This is a sensible default because most coroutines started by
+     * a [ViewModel] update the UI after performing some processing.
      */
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     private var tonight = MutableLiveData<SleepNight?>()
 
@@ -217,5 +226,13 @@ class SleepTrackerViewModel(
     }
 
     /**
+     * Called when the ViewModel is dismantled.
+     * At this point, we want to cancel all coroutines;
+     * otherwise we end up with processes that have nowhere to return to
+     * using memory and resources.
      */
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
