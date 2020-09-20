cd3641c220566d31d86fbf0c6ad3e0edc4ce3d9e
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-09-21 01:06:26 +0530

    Revert "Fix syntax error in SleepDatabaseDao.kt and SleepTrackerViewModel.kt"
    This reverts commit 097feaa7632d312671261b67fa4d181cbffa1b48.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -34,6 +34,13 @@ class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
 
+    /**
+     */
+
+    /**
+     *
+     *
+     */
 
     private var tonight = MutableLiveData<SleepNight?>()
 
@@ -47,56 +54,12 @@ class SleepTrackerViewModel(
     }
 
     /**
-     * If tonight has not been set, then the START button should be visible.
-     */
-    val startButtonVisible = Transformations.map(tonight) {
-        null == it
-    }
-
-    /**
-     * If tonight has been set, then the STOP button should be visible.
-     */
-    val stopButtonVisible = Transformations.map(tonight) {
-        null != it
-    }
-
-    /**
-     * If there are any nights in the database, show the CLEAR button.
-     */
-    val clearButtonVisible = Transformations.map(nights) {
-        it?.isNotEmpty()
-    }
-
-    /**
-     * Request a toast by setting this value to true.
-     *
-     * This is private because we don't want to expose setting this value to the Fragment.
-     */
-    private var _showSnackbarEvent = MutableLiveData<Boolean>()
-
-    /**
-     * If this is true, immediately `show()` a toast and call `doneShowingSnackbar()`.
-     */
-    val showSnackBarEvent: LiveData<Boolean>
-        get() = _showSnackbarEvent
-
-    /**
      * Variable that tells the Fragment to navigate to a specific [SleepQualityFragment]
      *
      * This is private because we don't want to expose setting this value to the Fragment.
      */
-
     private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
-    /**
-     * Call this immediately after calling `show()` on a toast.
-     *
-     * It will clear the toast request, so if the user rotates their phone it won't show a duplicate
-     * toast.
-     */
 
-    fun doneShowingSnackbar() {
-        _showSnackbarEvent.value = false
-    }
     /**
      * If this is non-null, immediately navigate to [SleepQualityFragment] and call [doneNavigating]
      */
@@ -138,16 +101,17 @@ class SleepTrackerViewModel(
             return night
     }
 
+
     private suspend fun clear() {
-        database.clear()
+            database.clear()
     }
 
     private suspend fun update(night: SleepNight) {
-        database.update(night)
+            database.update(night)
     }
 
     private suspend fun insert(night: SleepNight) {
-        database.insert(night)
+            database.insert(night)
     }
 
     /**
@@ -197,9 +161,8 @@ class SleepTrackerViewModel(
             // And clear tonight since it's no longer in the database
             tonight.value = null
         }
-
-        // Show a snackbar message, because it's friendly.
-        _showSnackbarEvent.value = true
     }
 
+    /**
+     */
 }
